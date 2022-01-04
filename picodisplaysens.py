3f6d5d9a6cfb41d16c4c3ecc49078b55aa3013a4
M3Stang <88910270+M3Stang@users.noreply.github.com>
2022-01-04 14:09:06 -0500

    Update picodisplaysens.py

--- picodisplaysens.py
+++ picodisplaysens.py
@@ -2,29 +2,38 @@ from machine import Pin, I2C
 import utime
 from ssd1306 import SSD1306_I2C
 
+#Declares all of the variables running on the pins.
 TRIG = Pin(3, Pin.OUT) 
 ECHO = Pin(2, Pin.IN)
 GLED = Pin(4, Pin.OUT)
 YLED = Pin(5, Pin.OUT)
 RLED = Pin(6, Pin.OUT)
+#Declares display width and height.
 WIDTH = 128
 HEIGHT = 32
 
+#Declares i2c bus and where the pins are located for the data line and clock.
 i2c = I2C(0, scl = Pin(17), sda = Pin(16), freq = 200000)
 
+#Declares the driver and display.
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
+#Empties the display
 oled.fill(0)
 
-oled.text("hello world", 0 , 0)
+#Prints booting up on the display to verify operation.
+oled.text("Booting up", 0 , 0)
 
+#Prints the text to the display.
 oled.show()
 
+#Console data.
 print ("Distance Measurement In Progress")
 
 try:
     while True:
-
+        
+        #Triggering the ultrasonic sensor to check distance.
         TRIG.low()
         print ("Waiting For Sensor To Settle")
         utime.sleep(2)
@@ -44,9 +53,11 @@ try:
         distance = (pulse_duration * 0.0343) / 2
 
         distance = round(distance, 2)
-
+        
+        #Prints distance to console.
         print ("Salt level in CM:", distance)
         
+        #Gets the Pico's temp and converts the data to a string.
         picoTemp = machine.ADC(4)
         convertFactor = 3.3 / (65535)
         reading = picoTemp.read_u16() * convertFactor
@@ -56,13 +67,14 @@ try:
         WIDTH = 128
         HEIGHT = 32
         
+        #Sends distance data to a string so that it can be printed to the display.
         disStr = str(distance)
         
         i2c = I2C(0, scl = Pin(17), sda = Pin(16), freq = 200000)
 
         oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
-
+        #Prints the data to the display for temp and distance.
         oled.fill(0)
         oled.text("Distance: " + disStr, 0 , 0)
         oled.text("RPi Temp: " + tempStr, 0, 10)
